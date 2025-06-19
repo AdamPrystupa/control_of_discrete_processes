a4d23f9000b7202d34405fcee9cb678ba96f3c59
Adam Prystupa <prystupaadam@gmail.com>
2025-06-19 11:52:42 +0200

    aktualizacja przed następną częścią

--- part2/main.cpp
+++ part2/main.cpp
@@ -8,56 +8,6 @@
 namespace fs = std::filesystem;
 
 int main(int argc, char *argv[]) {
-  // if (argc < 4) {
-  //   std::cerr << "Usage: " << argv[0]
-  //             << " <input_file> <algorithm> <number_of_machines>\n";
-  //   return 1;
-  // }
-
-  // std::string inputFile = argv[1];
-  // std::string algorithm = argv[2];
-  // std::string numberOfMachines = argv[3];
-
-  // std::vector<Task> tasks;
-  // std::vector<Machine> machines;
-  // createMachines(machines, std::stoi(numberOfMachines));
-
-  // int cmax = 0;
-
-  // loadTasksFromFile(inputFile, tasks);
-  // std::chrono::duration<double> elapsed;
-
-  // switch (algorithm[0]) {
-  // case 'p': {
-  //   auto start = std::chrono::high_resolution_clock::now();
-  //   lpt(tasks, machines);
-  //   auto end = std::chrono::high_resolution_clock::now();
-  //   elapsed = end - start;
-  //   break;
-  // }
-  // case 's': {
-  //   auto start = std::chrono::high_resolution_clock::now();
-  //   lsa(tasks, machines);
-  //   auto end = std::chrono::high_resolution_clock::now();
-  //   elapsed = end - start;
-  //   break;
-  // }
-  // default:
-  //   std::cerr << "Unknown algorithm: " << algorithm << "\n";
-  //   return 1;
-  // }
-  // printTasks(tasks);
-
-  // printMachines(machines);
-  // cmax = calculateCmax(machines);
-
-  // std::cout << "Cmax: " << cmax << "\n";
-  // std::cout
-  //     << "Execution time: "
-  //     <<
-  //     std::chrono::duration_cast<std::chrono::nanoseconds>(elapsed).count()
-  //     << " ns\n";
-
   std::vector<std::string> algorithms = {"lpt",  "lsa",   "pd",
                                          "ptas", "fptas", "opt"};
   std::chrono::duration<double> elapsed;
